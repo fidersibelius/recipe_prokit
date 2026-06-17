b1947294b58ac50b88a1db9e6c35f55921e02f47
Fidencio Javier Blanco Acosta <fidersibelius@gmail.com>
2026-06-17 00:43:17 -0600

    Base Recipe ProKit compilando

---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:nb_utils/nb_utils.dart';
+import 'package:recipe_prokit/screens/RCSplashScreen.dart';
+import 'package:recipe_prokit/store/AppStore.dart';
+import 'package:recipe_prokit/utils/AppTheme.dart';
+import 'package:recipe_prokit/utils/RCConstants.dart';
+import 'package:recipe_prokit/utils/RCDataGenerator.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+AppStore appStore = AppStore();
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await initialize(aLocaleLanguageList: languageList());
+
+  appStore.toggleDarkMode(value: getBoolAsync(isDarkModeOnPref));
+
+  defaultToastGravityGlobal = ToastGravity.BOTTOM;
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Observer(
+      builder: (_) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Sneaker Shopping${!isMobile ? ' ${platformName()}' : ''}',
+        home: RCSplashScreen(),
+
+        theme: !appStore.isDarkModeOn
+            ? AppThemeData.lightTheme
+            : AppThemeData.darkTheme,
+
+        navigatorKey: navigatorKey,
+        scrollBehavior: SBehavior(),
+
+        // YA EXISTIA
+        supportedLocales: LanguageDataModel.languageLocales(),
+
+        // AGREGA ESTO
+        localizationsDelegates: const [
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+
+        localeResolutionCallback: (locale, supportedLocales) => locale,
+      ),
+    );
+  }
+}

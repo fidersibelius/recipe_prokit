69d9f359144dd3f4a6144ecbabaa176f8c9939d4
Fidencio Javier Blanco Acosta <fidersibelius@gmail.com>
2026-07-25 12:33:04 -0600

    feat: add QR sharing for generated tickets

--- lib/modules/developer/screens/DeveloperQrScreen.dart
+++ lib/modules/developer/screens/DeveloperQrScreen.dart
@@ -3,6 +3,9 @@ import 'dart:typed_data';
 import 'package:flutter/material.dart';
 
 import '../../../shared/widgets/BTScaffold.dart';
+import 'package:share_plus/share_plus.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
 
 class DeveloperQrScreen extends StatelessWidget {
   final Uint8List qrBytes;
@@ -17,10 +20,38 @@ class DeveloperQrScreen extends StatelessWidget {
     return BTScaffold(
       title: "Boleto creado",
       body: Center(
-        child: Image.memory(
-          qrBytes,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.memory(
+              qrBytes,
+            ),
+            const SizedBox(height: 30),
+            ElevatedButton.icon(
+              onPressed: compartirQr,
+              icon: const Icon(Icons.share),
+              label: const Text("Compartir"),
+            ),
+          ],
         ),
       ),
+    );
+  }
+
+  Future<void> compartirQr() async {
+    final tempDir = await getTemporaryDirectory();
+
+    final file = File(
+      '${tempDir.path}/boleto_qr.png',
+    );
+
+    await file.writeAsBytes(qrBytes);
+
+    await Share.shareXFiles(
+      [
+        XFile(file.path),
+      ],
+      text: 'Tu boleto para el evento.',
     );
   }
 }
